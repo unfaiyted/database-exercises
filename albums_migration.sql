e20a6fd51cbaf141d3e7caba31df68180482c1ac
unfaiyted <unfaiyted@gmail.com>
2018-04-14 16:53:05 -0500

    adding albums sql

---
+++ albums_migration.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+
+  id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  artist VARCHAR(100),
+  name VARCHAR(200),
+  release_date DATE NULL,
+  sales DECIMAL(10,2),
+  genre VARCHAR(50)
+
+);
