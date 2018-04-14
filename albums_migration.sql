6db3fa3a0683688513a02663378975ee8712e24a
unfaiyted <unfaiyted@gmail.com>
2018-04-14 17:37:33 -0500

    code uses multiple inserts. converted albumn data from wiki to json

--- albums_migration.sql
+++ albums_migration.sql
@@ -7,7 +7,7 @@ CREATE TABLE albums (
   id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
   artist VARCHAR(100),
   name VARCHAR(200),
-  release_date DATE NULL,
+  release_date YEAR NULL,
   sales DECIMAL(10,2),
   genre VARCHAR(50)
 
